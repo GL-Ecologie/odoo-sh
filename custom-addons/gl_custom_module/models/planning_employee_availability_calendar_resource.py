3fc39f17db3dd90d3ac6e96ed5bc4e2fb25b5ad4
Julian Ruiz Burgos <git@julianruizburgos.net>
2026-03-11 17:48:37 +0100

    fix get_calendar_filters

--- custom-addons/gl_custom_module/models/planning_employee_availability_calendar_resource.py
+++ custom-addons/gl_custom_module/models/planning_employee_availability_calendar_resource.py
@@ -21,7 +21,31 @@ class PlanningEmployeeAvailabilityCalendarResource(models.Model):
     checked = fields.Boolean(default=True)
 
     def get_calendar_filters(self, user_id, field_names):
-        
-        if not self.env['planning.calendar.resource'].search_count([('user_id', '=', user_id), ('resource_id', '=', False)]):
-            self.env['planning.calendar.resource'].create({'resource_id': False, 'user_id': user_id})
-        return self.search_read(domain=[('user_id', '=', user_id)], fields=field_names)
+        user = self.env["res.users"].browse(user_id)
+
+        if user.has_group("planning.group_planning_manager"):
+            target_resources = self.env["hr.employee"].search([("active", "=", True)]).mapped("resource_id").filtered(lambda r: r)
+        else:
+            employee = self.env["hr.employee"].search([("user_id", "=", user_id)], limit=1)
+            target_resources = employee.resource_id if employee and employee.resource_id else self.env["resource.resource"]
+
+        existing_resource_ids = set(
+            self.search([("user_id", "=", user_id)]).mapped("resource_id").ids
+        )
+
+        missing_vals = []
+        for resource in target_resources:
+            if resource.id not in existing_resource_ids:
+                missing_vals.append({
+                    "user_id": user_id,
+                    "resource_id": resource.id,
+                    "checked": not user.has_group("planning.group_planning_manager"),
+                })
+
+        if missing_vals:
+            self.create(missing_vals)
+
+        return self.search_read(
+            domain=[("user_id", "=", user_id)],
+            fields=field_names,
+        )
