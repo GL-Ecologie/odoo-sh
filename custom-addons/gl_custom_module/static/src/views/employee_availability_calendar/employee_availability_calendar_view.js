0094b19b1b890b63803cb1d33c228b3d9fdf8efb
Julian Ruiz Burgos <git@julianruizburgos.net>
2026-03-12 11:21:56 +0100

    fix availability calendar custom rendering

--- custom-addons/gl_custom_module/static/src/views/employee_availability_calendar/employee_availability_calendar_view.js
+++ custom-addons/gl_custom_module/static/src/views/employee_availability_calendar/employee_availability_calendar_view.js
@@ -1,6 +1,7 @@
 import { registry } from "@web/core/registry";
 import { calendarView } from "@web/views/calendar/calendar_view";
 import { CalendarController } from "@web/views/calendar/calendar_controller";
+import { CalendarRenderer } from "@web/views/calendar/calendar_renderer";
 import { Component } from "@odoo/owl";
 
 class AvailabilityLegend extends Component {
@@ -14,9 +15,32 @@ class EmployeeAvailabilityCalendarController extends CalendarController {
     };
 }
 
+class EmployeeAvailabilityCalendarRenderer extends CalendarRenderer {
+    get options() {
+        const options = super.options;
+        const superEventDidMount = options.eventDidMount;
+
+        options.eventDidMount = (info) => {
+            if (superEventDidMount) {
+                superEventDidMount(info);
+            }
+
+            info.el.classList.add("o_availability_event");
+
+            const styleKey = info.event.extendedProps?.style_key;
+            if (styleKey) {
+                info.el.classList.add(`o_availability_${styleKey}`);
+            }
+        };
+
+        return options;
+    }
+}
+
 export const employeeAvailabilityCalendarView = {
     ...calendarView,
     Controller: EmployeeAvailabilityCalendarController,
+    Renderer: EmployeeAvailabilityCalendarRenderer,
 };
 
 registry.category("views").add("employee_availability_calendar", employeeAvailabilityCalendarView);
