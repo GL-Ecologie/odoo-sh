992ceb4ed8f43c8fd379e82161917a4939cff351
Julian Ruiz Burgos <git@julianruizburgos.net>
2026-03-24 14:05:00 +0100

    fix buttons

--- custom-addons/gl_custom_module/models/planning_slot.py
+++ custom-addons/gl_custom_module/models/planning_slot.py
@@ -407,6 +407,11 @@ class PlanningSlot(models.Model):
             weekends_conflict = True
         return weekends_conflict
 
+    @api.model
+    def action_open_multi_assign_create_wizard(self):
+        """Open the multi-resource shift creation wizard."""
+        return self.env["planning.multi_assign_wizard"].action_open_create_wizard()
+
     def action_register_timesheet(self):
             """Create a pre-filled timesheet entry for this shift and open it."""
             self.ensure_one()
